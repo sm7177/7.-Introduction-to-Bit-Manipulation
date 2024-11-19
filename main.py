5f1c3cd170f1289a6a9a5c1e4939a654dcfa504d
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-19 12:32:52 +0545

    .

---
+++ main.py
@@ -0,0 +1,14 @@
+num1=20
+num2=10
+
+print("AND operator", num1&num2)
+
+print("OR operator", num1|num2)
+
+print("XOR operator", num1^num2)
+
+print("NOT operator", ~num1)
+
+print("Left shift operator",num1 << 1)
+
+print("Right shift operator", num1 >> 1)
